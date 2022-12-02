05e0be1bb1e470f97f0eacdc0f0536ae80d299e6
Daylin Morgan <daylinmorgan@gmail.com>
2022-12-02 16:53:38 -0600

    2022-day2: part two complete

--- 2022/002/main.go
+++ 2022/002/main.go
@@ -16,17 +16,48 @@ func check(e error) {
 // A/X -> Rock
 // B/Y -> Paper
 // C/Z -> Scissors
-func scoreRound(myPlay string, oppPlay string) int {
-	scoreKey := map[string]string{"X": "C", "Z": "B", "Y": "A"}
+//
+// Part One Scoring
+// func scoreRound(myPlay string, oppPlay string) int {
+// 	scoreKey := map[string]string{"X": "C", "Z": "B", "Y": "A"}
+// 	scorer := map[string]int{"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+//
+// 	if scorer[myPlay] == scorer[oppPlay] {
+// 		return scorer[myPlay] + 3
+// 	} else if scoreKey[myPlay] == oppPlay {
+// 		return scorer[myPlay] + 6
+// 	} else {
+// 		return scorer[myPlay]
+// 	}
+// }
+
+// X -> lose
+// Y -> draw
+// Z -> win
+
+func reverseMap(m map[string]string) map[string]string {
+	n := make(map[string]string, len(m))
+	for k, v := range m {
+		n[v] = k
+	}
+	return n
+}
+
+func scoreRound(outcome string, play string) int {
+	var score int
+	loseKey := map[string]string{"A": "C", "B": "A", "C": "B"}
+	winKey := reverseMap(loseKey)
 	scorer := map[string]int{"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
 
-	if scorer[myPlay] == scorer[oppPlay] {
-		return scorer[myPlay] + 3
-	} else if scoreKey[myPlay] == oppPlay {
-		return scorer[myPlay] + 6
-	} else {
-		return scorer[myPlay]
+	switch outcome {
+	case "X":
+		score = scorer[loseKey[play]]
+	case "Y":
+		score = scorer[play] + 3
+	case "Z":
+		score = scorer[winKey[play]] + 6
 	}
+	return score
 }
 
 func main() {
