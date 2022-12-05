50f4c8016fdf14c68f456198b8a591f40026c9ee
Daylin Morgan <daylinmorgan@gmail.com>
2022-12-04 18:17:41 -0600

    2022-day3: part two complete

--- 2022/003/main.go
+++ 2022/003/main.go
@@ -7,6 +7,19 @@ import (
 	"unicode"
 )
 
+// https://stackoverflow.com/a/69477128
+func chunkSlice(items []string, chunkSize int32) (chunks [][]string) {
+	//While there are more items remaining than chunkSize...
+	for chunkSize < int32(len(items)) {
+		//We take a slice of size chunkSize from the items array and append it to the new array
+		chunks = append(chunks, items[0:chunkSize])
+		//Then we remove those elements from the items array
+		items = items[chunkSize:]
+	}
+	//Finally we append the remaining items to the new array and return it
+	return append(chunks, items)
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -23,19 +36,36 @@ func readFileLines(filename string) []string {
 	return strings.FieldsFunc(string(dat), splitFn)
 }
 
-func getSharedItem(bag string) rune {
-	size := len(bag)
-	comp1, comp2 := bag[:size/2], bag[size/2:]
+// part one
+//
+//	func getSharedItem(bag string) rune {
+//		size := len(bag)
+//		comp1, comp2 := bag[:size/2], bag[size/2:]
+//		var sharedType rune
+//		for _, c1 := range comp1 {
+//			for _, c2 := range comp2 {
+//				if c1 == c2 {
+//					sharedType = c1
+//				}
+//			}
+//		}
+//		return sharedType
+//	}
+
+func getSharedItem(bags []string) rune {
+	// size := len(bag)
+	// comp1, comp2 := bag[:size/2], bag[size/2:]
 	var sharedType rune
-	for _, c1 := range comp1 {
-		for _, c2 := range comp2 {
-			if c1 == c2 {
-				sharedType = c1
+	for _, c1 := range bags[0] {
+		for _, c2 := range bags[1] {
+			for _, c3 := range bags[2] {
+				if c1 == c2 && c2 == c3 {
+					sharedType = c1
+				}
 			}
 		}
 	}
 	return sharedType
-
 }
 
 func priorityKey() map[rune]int {
@@ -56,10 +86,12 @@ func main() {
 	priorities := priorityKey()
 	prioritySum := 0
 
+	// bags := readFileLines("input-head.txt")
 	bags := readFileLines("input.txt")
+	elfGroups := chunkSlice(bags, 3)
 
-	for _, bag := range bags {
-		prioritySum += priorities[getSharedItem(bag)]
+	for _, bags := range elfGroups {
+		prioritySum += priorities[getSharedItem(bags)]
 	}
 	fmt.Println("Sum of priorities is", prioritySum)
 }
